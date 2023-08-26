1f9e2f605eefa8592a4f8b945138b66803fe2964
Oscar <a20193315@pucp.edu.pe>
2023-08-25 19:51:28 -0500

    probando ramas

---
+++ lab1-20193315/src/main/java/com/example/lab120193315/Lab120193315Application.java
@@ -0,0 +1,13 @@
+package com.example.lab120193315;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab120193315Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab120193315Application.class, args);
+    }
+
+}
